33ff64238c5d8e1ed1f3863a274c2e5ba501f69c
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-19 01:31:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+  end
+
+  get "/hometown" do
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Mein Herz Brennt"
+  end
 end
